f4eb9798c4bfd1c05bdf2dc5fccf85a31efd03b9
Aleutoto <163339683+Aleutoto@users.noreply.github.com>
2024-05-22 05:43:25 +0300

    improve variables

--- index.js
+++ index.js
@@ -1,37 +1,46 @@
 const express = require('express');
 require('dotenv').config();
+
 const app = express();
 app.use(express.json());
+
 const PORT = process.env.PORT || 3000;
-let tasks = [];
+let taskList = [];
+
 app.post('/tasks', (req, res) => {
-    const task = {
-        id: tasks.length + 1,
+    const newTask = {
+        id: taskList.length + 1,
         description: req.body.description,
-        completed: false
+        isCompleted: false
     };
-    tasks.push(task);
-    res.status(201).send(task);
+    taskList.push(newTask);
+    res.status(201).send(newTask);
 });
+
 app.get('/tasks', (req, res) => {
-    res.status(200).send(tasks);
+    res.status(200).send(taskList);
 });
+
 app.get('/tasks/:id', (req, res) => {
-    const task = tasks.find(t => t.id === parseInt(req.params.id));
-    if (!task) return res.status(404).send('Task not found');
+    const task = taskList.find(task => task.id === parseInt(req.params.id));
+    if (!task) return res.status(404).send('Task not found.');
     res.send(task);
 });
+
 app.put('/tasks/:id', (req, res) => {
-    const task = tasks.find(t => t.id === parseInt(req.params.id));
-    if (!task) return res.status(404).send('Task not found');
-    task.description = req.body.description;
-    task.completed = req.body.completed;
+    let task = taskList.find(task => task.id === parseInt(req.params.id));
+    if (!task) return res.status(404).send('Task not found.');
+    
+    task.description = req.body.description ?? task.description;
+    task.isCompleted = req.body.completed ?? task.isCompleted;
     res.send(task);
 });
+
 app.delete('/tasks/:id', (req, res) => {
-    const taskIndex = tasks.findIndex(t => t.id === parseInt(req.params.id));
-    if (taskIndex === -1) return res.status(404).send('Task not found');
-    tasks.splice(taskIndex, 1);
+    const taskIndex = taskList.findIndex(task => task.id === parseInt(req.params.id));
+    if (taskIndex === -1) return res.status(404).send('Task not found.');
+    taskList.splice(taskIndex, 1);
     res.status(204).send();
 });
+
 app.listen(PORT, () => console.log(`Server is running on http://localhost:${PORT}`));
