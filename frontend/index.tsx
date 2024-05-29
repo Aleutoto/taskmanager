245ed5b67450c9df1e93a9f7dc2ccc54c18c6b5e
Aleutoto <163339683+Aleutoto@users.noreply.github.com>
2024-05-29 05:38:49 +0300

    improve code readability

--- frontend/index.tsx
+++ frontend/index.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-const rootElement = document.getElementById('root');
-if (rootElement) {
-  ReactDOM.createRoot(rootElement).render(
+
+const rootDomElement = document.getElementById('root');
+
+if (rootDomElement) {
+  const root = ReactDOM.createRoot(rootDomElement);
+  root.render(
     <React.StrictMode>
       <App />
     </React.StrictMode>,
